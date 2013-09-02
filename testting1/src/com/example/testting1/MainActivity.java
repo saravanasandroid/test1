4ac2f8e0a05e714eb88d2eab04cf23912e001081
saravana <saravanakumar@n-school.com>
2013-09-02 17:58:25 +0530

    changed 2 line

--- testting1/src/com/example/testting1/MainActivity.java
+++ testting1/src/com/example/testting1/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         Log.i("Hey","");
+        Log.i("check","");
     }
 
     @Override
